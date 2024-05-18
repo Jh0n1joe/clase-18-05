16daf868044637c50d5581f1b16eadd18664f8d3
Jhony.j <jhonycrq1816@gmail.com>
2024-05-18 12:20:50 -0400

    detail added

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView
 from .models import Post
+from django.views.generic import DetailView
 
 # Create your views here.
 class BlogListView(ListView):
@@ -11,3 +12,7 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = "post_new.html"
     fields = ["title", "author", "body"]
+
+class BlogDetailView(DetailView):
+        model = Post
+        template_name = "post_detail.html"
